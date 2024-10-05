b7d70bf6564faa78dd215b13b3e5700744ad3392
Oleg <oleg.arhipovski@gmail.com>
2024-10-05 22:08:47 +0300

    First commit

---
+++ src/main/java/aoleg/com/github/audiomanager/AudioManagerApplication.java
@@ -0,0 +1,13 @@
+package aoleg.com.github.audiomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aoleg/com/github/audiomanager/AudioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package aoleg.com.github.audiomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
